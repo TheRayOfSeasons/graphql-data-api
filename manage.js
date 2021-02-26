62534588757c892df2ca7354a2810065edfec63d
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2021-02-26 13:58:13 +0800

    Revert: manage js as syncronous

--- manage.js
+++ manage.js
@@ -2,7 +2,5 @@ import { commands } from './management/commands';
 
 const [command, ...options] = process.argv.slice(2);
 console.log(`Running ${command}`);
-(async () => {
-  const output = await commands[command](...options);
-  console.log(output);
-});
+const output = commands[command](...options);
+console.log(output);
